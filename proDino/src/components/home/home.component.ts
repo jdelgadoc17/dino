3b65bfade49c8579817f92a080f87b9f5a2ad94d
Javier Delgado Chacón <jdelgadoc17@iesagora.caceres>
2024-10-30 17:12:15 +0100

    push added

--- proDino/src/components/home/home.component.ts
+++ proDino/src/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DinosaurService } from '../../services/dinosaurservice.service';
 import { EnclosureService } from '../../services/enclosureservice.service';
+import { ParkService } from '../../services/parkservice.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -16,11 +17,14 @@ export class HomeComponent implements OnInit {
   clicksToNextUnlock: number = 10;
   message: string = '';
   dinosaurs: any[] = [];
-  enclosures: any[] = []; 
+  enclosures: any[] = [];
+  dinosauriosComprados: number[] = []; 
+  recintosComprados: number[] = []; 
 
   constructor(
     private dinosaurService: DinosaurService,
-    private enclosureService: EnclosureService
+    private enclosureService: EnclosureService,
+    private parkService: ParkService
   ) {}
 
   ngOnInit() {
@@ -30,6 +34,12 @@ export class HomeComponent implements OnInit {
 
     this.enclosureService.getEnclosures().subscribe(data => {
       this.enclosures = data;
+    });
+
+    this.parkService.getParkStatus().subscribe(data => {
+      this.coins = data.coins;
+      this.dinosauriosComprados = data.dinosaurIds || [];
+      this.recintosComprados = data.recintosIds || [];
     });
   }
 
@@ -47,14 +57,30 @@ export class HomeComponent implements OnInit {
   buyDinosaur(dinosaur: any) {
     if (this.coins >= dinosaur.cost) {
       this.coins -= dinosaur.cost;
+      this.dinosauriosComprados.push(dinosaur.id); 
       this.message = `Has comprado un ${dinosaur.name}.`;
+      this.updatePark(); 
     }
   }
 
   buyEnclosure(enclosure: any) {
     if (this.coins >= enclosure.cost) {
       this.coins -= enclosure.cost;
+      this.recintosComprados.push(enclosure.id); 
       this.message = `Has comprado el recinto ${enclosure.name}.`;
+      this.updatePark(); 
     }
+  }
+
+  updatePark() {
+    const data = {
+      coins: this.coins,
+      dinosaurIds: this.dinosauriosComprados,
+      recintosIds: this.recintosComprados,
+    };
+
+    this.parkService.updatePark(data).subscribe(response => {
+      console.log('Parque actualizado:', response);
+    });
   }
 }
